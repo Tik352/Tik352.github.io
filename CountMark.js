079e2cc011649b74082bf066205bb824d8dd2495
Tik352 <realbean352@gmail.com>
2017-10-27 18:49:36 +0400

    Update CountMark.js

--- CountMark.js
+++ CountMark.js
@@ -13,7 +13,7 @@ function CountMark(){
 			var first_semester = 0.4 * mark1 + exam_mark;
 			
 			
-			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester + " ~ " + Math.round(first_semester);			
+			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester.toFixed(3) + " ~ " + Math.round(first_semester);			
 			break;
 		case "matan.html":
 			var test_mark = document.getElementById('test1').value * 0.7; // Оценка контрольную
@@ -22,7 +22,7 @@ function CountMark(){
 			
 			var first_semester = exam_mark + 0.3 * (test_mark + hw_mark);
 			
-			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester + " ~ " + Math.round(first_semester);						
+			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester.toFixed(3) + " ~ " + Math.round(first_semester);						
 			break;
 		case "diskra.html":
 			var hw1 = document.getElementById('hw1').value;
@@ -34,7 +34,7 @@ function CountMark(){
 			var current_control = (D12 < test12) ? ( 0.62 * D12 + 0.38 * test12) : ( 0.62 * test12 + 0.38 * D12);
 	
 			var first_semester = 0.8 * current_control + 0.2 * document.getElementById('coursera').value;
-			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester + " ~ " + Math.round(first_semester);					
+			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester.toFixed(3) + " ~ " + Math.round(first_semester);					
 			break;
 		case "proga.html":
 			var no1 = document.getElementById('test1').value * 0.8  + document.getElementById('current_check1').value * 0.2; // Накоп за 1й модуль
@@ -47,7 +47,7 @@ function CountMark(){
 			
 			var first_semester = 0.8 * exam_mark + 0.2 * no;
 			
-			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester + " ~ " + Math.round(first_semester);			
+			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester.toFixed(3) + " ~ " + Math.round(first_semester);			
 			break;	
 		case "vvpi.html":
 			var tz_mark = document.getElementById('tz').value * 0.6;
@@ -57,7 +57,7 @@ function CountMark(){
 			
 			var O = tz_mark + plan_mark + scheme_mark;
 			var first_semester = O * 0.6 + exam_mark;
-			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester + " ~ " + Math.round(first_semester);			
+			document.getElementById('mark').innerHTML = "Ваша оценка: " +first_semester.toFixed(3) + " ~ " + Math.round(first_semester);			
 			break;
 	}
 }
