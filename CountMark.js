952a5c5f751f0bc811c617754a454745433ffd98
Tik352 <realbean352@gmail.com>
2018-02-08 17:44:38 +0300

    Update CountMark.js

--- CountMark.js
+++ CountMark.js
@@ -11,7 +11,7 @@ function CountMark(){
 			var exam_mark = document.getElementById('exam').value * 0.3; // оценка за экзамен 
 			
 			var mark1 = test_mark + hw_mark + sem_mark + colloc;
-			var first_semester = 0.4 * mark1 + exam_mark;
+			var first_semester = 0.7 * mark1 + exam_mark;
 			
 			
 			document.getElementById('mark').innerHTML = first_semester.toFixed(2) + " ~ " + Math.round(first_semester);			
