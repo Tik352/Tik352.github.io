67282b452134bacc4a9ff08a4f870e592f7659d5
Tik352 <realbean352@gmail.com>
2018-02-12 15:35:02 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,11 @@
 <?php
   $text = "ничего нет";
+  $another_text = "и тут тоже";
   if(isset($_GET['text'])){
     $text = $_GET['text'];
   }
-  echo "$text <br> Идите нахуй пожалуйста";
+  if(isset($_GET['another_text'])){
+    $another_text = $_GET['another_text'];
+  }
+  echo "$text <br> Идите нахуй пожалуйста <br> $another_text <br> да ты охуел";
 ?>
