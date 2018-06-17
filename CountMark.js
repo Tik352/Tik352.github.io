0af789f3332604102b0b010fd46e553c850a7b51
Tik352 <realbean352@gmail.com>
2018-06-18 00:37:21 +0400

    Update CountMark.js
    обновил формулу оценки по проге

--- CountMark.js
+++ CountMark.js
@@ -28,7 +28,7 @@ function CountMark(){
 			break;
 		case "diskra.html":
 			var hw3 = document.getElementById('hw3').value;
-			var hw4 = document.getElementById('hw4').value;
+			var hw4 = document.getElementById('hw4').value; 
 			var D34 = (hw3 < hw4) ? ( 0.62 * hw3 + 0.38 * hw4) : ( 0.62 * hw3 + 0.38 * hw4);
 			var test3 =  document.getElementById('test3').value;
 			var test4 =  document.getElementById('test4').value;
@@ -39,12 +39,12 @@ function CountMark(){
 			document.getElementById('mark').innerHTML = first_semester.toFixed(2) + " ~ " + Math.round(first_semester);		
 			break;
 		case "proga.html":
-			var no1 = document.getElementById('test1').value * 0.8  + document.getElementById('current_check1').value * 0.2; // Накоп за 1й модуль
+			var no1 = document.getElementById('hw').value * 0.5 + document.getElementById('current_check1').value * 0.5; // Накоп за 3й модуль
 			
-			var no2 = document.getElementById('hw').value * 0.35 +  document.getElementById('test2').value * 0.35 + document.getElementById('current_check2').value * 0.3; // накоп за второй модуль
-			
+			var no2 = document.getElementById('test2').value * 0.8 + document.getElementById('current_check2').value * 0.2; // накоп за 4 модуль
+			// ПИДАРАСЫ
 			var no =  no1 * 0.2 + no2 * 0.8;
-			
+			// ОХУЕННЫЙ МИН МАКС АЩАЩА БЛЯТЬ
 			var exam_mark = Math.min(document.getElementById('podbel_test').value, document.getElementById('exam_control_task').value) * 0.8 + Math.max(document.getElementById('podbel_test').value, document.getElementById('exam_control_task').value) * 0.2;
 			
 			var first_semester = 0.8 * exam_mark + 0.2 * no;
